39bda3018e929bc9525d985f9b8cfa6fc83fe509
marcotrunc <marcotrunc@gmail.com>
2022-01-12 15:24:06 +0100

    completed the logic in js

--- js/script.js
+++ js/script.js
@@ -12,12 +12,25 @@ const generateElement = document.getElementById('generate');
 const deleteElement = document.getElementById('delete');
 // ** Richiamare Elementi in pagina (km da percorrere, fascia d'età, cost-ticket)
 let kmElement = document.querySelector('#km');
-let ageElement = document.querySelector('#age option');
-let costTicket = document.getElementById('cost-ticket'); 
+let ageElement = document.getElementById('age');
+let costElement = document.getElementById('cost-ticket');
+
+let ultimatePrice;
 // ** Quando premo su Genera Acquisisco i valori
 generateElement.addEventListener('click', function(){
     kmElement = kmElement.value;
     ageElement = ageElement.value
+    const originalPrice = kmElement * 0.21;
+    console.log(`kmElement: ${kmElement}`);
+    console.log(`ageElement: ${ageElement}`);
+    console.log(`Il prezzo originale è: ${originalPrice}`);
+    if(ageElement === 'nodiscount'){
+        ultimatePrice = originalPrice;
+    }else if(ageElement === 'minorenni'){
+        ultimatePrice = originalPrice - (originalPrice * 0.20);
+    }else if(ageElement ==='over 65'){
+        ultimatePrice = originalPrice - (originalPrice * 0.40);
+    }
+    console.log(`Il costo definitivo del biglietto è: ${ultimatePrice}`);
+    costElement.innerHTML = `<strong>Il costo definitivo del biglietto è ${ultimatePrice}€`;
 });
-console.log(kmElement);
-console.log(ageElement);
